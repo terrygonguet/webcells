13d950409d09c1a44de07a4f817c9a8512ca9ed0
terrygonguet <terry@gonguet.com>
2021-06-12 08:43:57 +0200

    Add random page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,8 +10,9 @@
 <main in:fade={fadeIn} out:fly={flyOutUp}>
 	<h1 class="text-[7rem] font-thin uppercase">Webcells</h1>
 
-	<nav class="my-auto grid grid-cols-2 gap-10">
+	<nav class="my-auto grid grid-cols-1 md:grid-cols-3 gap-10">
 		<a class="btn" href="/custom" sveltekit:prefetch>Play custom level</a>
+		<a class="btn" href="/random" sveltekit:prefetch>Play random level</a>
 		<a class="btn" href="/editor" sveltekit:prefetch>Level editor</a>
 	</nav>
 </main>
@@ -19,5 +20,10 @@
 <style lang="postcss">
 	main {
 		@apply flex flex-col items-center py-12;
+	}
+
+	/* ! HACK - text-[7rem] is broken rn */
+	h1 {
+		font-size: 7rem;
 	}
 </style>
---
+++ src/routes/random.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { game } from "$lib/action"
+	import { parse } from "$lib/game"
+	import { fadeIn, fadeOut, flyInDown } from "$lib/transition"
+	import { fade, fly } from "svelte/transition"
+
+	const level = parse("")
+</script>
+
+<svelte:head>
+	<title>Random Level - Webcells</title>
+</svelte:head>
+
+<main class="grid overflow-hidden" in:fly|local={flyInDown} out:fly|local={fadeOut}>
+	<canvas
+		in:fade={fadeIn}
+		out:fade={fadeOut}
+		class="w-full h-full col-start-1 row-start-1"
+		use:game={{ level }}
+	>
+		Your browser is not supported, please use <a
+			href="https://www.mozilla.org/en-US/firefox/new/"
+			target="_blank">Firefox</a
+		> or another evergreen browser.
+	</canvas>
+</main>
