af3f6ad61fd4fefb8ba8ee896016d1c08d0f04b0
terrygonguet <terry@gonguet.com>
2021-06-12 08:31:25 +0200

    Cool background is not worth it

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-	import { randInt } from "$lib/utils"
-	import { onMount } from "svelte"
 	import "../global.css"
-
-	function wavy() {
-		const svelte = document.getElementById("svelte")
-		if (!svelte) return
-		svelte.style.backgroundPositionX = randInt(0, 40) + "%"
-		svelte.style.backgroundPositionY = randInt(0, 40) + "%"
-	}
-
-	onMount(() => {
-		const id = setInterval(wavy, 2000)
-		wavy()
-		return () => clearInterval(id)
-	})
 </script>
 
 <slot />
