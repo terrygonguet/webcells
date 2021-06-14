a762c445ec6dc41fed6533105f1f2024f067feca
terrygonguet <terry@gonguet.com>
2021-06-14 11:39:37 +0200

    More buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,17 +11,19 @@
 	<h1 class="text-[9rem] font-thin uppercase">Webcells</h1>
 
 	<nav class="contents">
-		<div class="my-auto nav">
+		<div class="my-auto nav" style="--n-cols: 2">
 			<a class="btn" href="/tutorial" sveltekit:prefetch>Tutorial</a>
-			<a class="btn" href="/custom" sveltekit:prefetch>Play custom level</a>
+			<a class="btn" href="/custom" sveltekit:prefetch>Custom level</a>
+			<a class="btn" href="/reddit" sveltekit:prefetch>Reddit levels</a>
 			<button class="btn flex-col" href="/random" disabled
-				>Play random level<span class="text-sm">Coming soon</span></button
+				>Random level<span class="text-sm">Coming soon</span></button
 			>
 		</div>
 
-		<div class="nav" style="--n-cols: 2">
+		<div class="nav" style="--n-cols: 3">
 			<button class="btn" href="/editor" disabled>Level editor</button>
 			<a class="btn" href="/settings" sveltekit:prefetch>Settings</a>
+			<a class="btn" href="/about" sveltekit:prefetch>About</a>
 		</div>
 	</nav>
 </main>
@@ -50,6 +52,7 @@
 
 		.nav {
 			grid-template-columns: repeat(var(--n-cols, 3), 1fr);
+			grid-auto-rows: 1fr;
 		}
 	}
 </style>
