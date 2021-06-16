fdf336dc70ecad52b3a056182615f64e2be445cc
terrygonguet <terry@gonguet.com>
2021-06-17 00:47:18 +0200

    Much better reddit level list

--- src/routes/reddit/index.svelte
+++ src/routes/reddit/index.svelte
@@ -15,51 +15,133 @@
 	import { fadeOut, flyInDown } from "$lib/transition"
 	import { fly } from "svelte/transition"
 	import type { Puzzle } from "./levels.json"
+	import { page } from "$app/stores"
+	import { browser } from "$app/env"
+
+	type Entry = [string, string, string, string]
 
 	export let puzzles: Puzzle[]
 
-	$: entries = puzzles.map(({ title, id, user }) => [id, ...parseTitle(title), user])
+	let searchTitle = $page.query.get("title") ?? "",
+		searchDifficulty = $page.query.get("difficulty") ?? "*"
 
-	function parseTitle(title: string) {
+	$: entries = puzzles.map<Entry>(({ title, id, user }) => [id, ...parseTitle(title), user])
+	$: difficulties = getUniqueDifficulties(entries)
+	$: filteredEntries = entries.filter(
+		([, title, difficulty]) =>
+			title.toLocaleLowerCase().includes(searchTitle.toLocaleLowerCase()) &&
+			(searchDifficulty == "*" || difficulty == searchDifficulty),
+	)
+	$: browser && updateQuery(searchTitle, searchDifficulty)
+
+	function updateQuery(title: string, difficulty: string) {
+		title == "" ? $page.query.delete("title") : $page.query.set("title", title)
+		difficulty == "*" ? $page.query.delete("difficulty") : $page.query.set("difficulty", difficulty)
+		let queryString = $page.query.toString()
+		history.replaceState(null, "", "/reddit" + (queryString ? "?" + queryString : ""))
+	}
+
+	function parseTitle(title: string): [string, string] {
 		const result = /^\[.+?\](.+?)(?:[([](.+?)[\])])?$/.exec(title)
-		if (!result) return []
-		result.shift()
-		return result as string[]
+		if (!result) return ["Untitled", "?"]
+		return [result[1], slugifyDifficulty(result[2] || "?")]
+	}
+
+	function getUniqueDifficulties(entries: Entry[]) {
+		const set = new Set<string>()
+		entries.forEach(([, , difficulty]) => set.add(difficulty))
+		return Array.from(set).sort()
+	}
+
+	function slugifyDifficulty(difficulty: string) {
+		return difficulty.toLocaleLowerCase().trim().replace(/-|\//g, " / ").replace("] [", " & ")
 	}
 </script>
 
-<main
-	class="flex flex-col items-center py-12 text-xl"
-	in:fly|local={flyInDown}
-	out:fly|local={fadeOut}
->
-	<table>
-		<thead>
-			<tr>
-				<th class="px-4 py-2 border-b border-white text-left">Title</th>
-				<th class="px-4 py-2 border-b border-white">Difficulty</th>
-				<th class="px-4 py-2 border-b border-white">Submitted by</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each entries as [id, title, difficulty, user]}
-				<tr class="border-b border-white transition-transform transform hover:scale-110">
-					<td>
-						<a href="/reddit/{id}" class="w-full px-4 py-2 flex my-1">{title}</a>
-					</td>
-					<td>
-						<a href="/reddit/{id}" class="w-full px-4 py-2 flex justify-center my-1">{difficulty}</a
-						>
-					</td>
-					<td>
-						<a href="/reddit/{id}" class="w-full px-4 py-2 flex justify-center my-1">{user}</a>
-					</td>
-				</tr>
-			{:else}
-				<tr>
-					<td colspan="2">No puzzles...</td>
-				</tr>
+<main in:fly|local={flyInDown} out:fly|local={fadeOut}>
+	<form on:submit|preventDefault>
+		<img
+			class="h-8 self-center mr-1 transform"
+			style="--tw-scale-x:-1"
+			src="/svg/search.svg"
+			alt="Search icon"
+		/>
+		<input
+			bind:value={searchTitle}
+			type="text"
+			class="text-black text-md border-2 border-pink-600 px-2 py-1 flex-1"
+		/>
+		<select
+			bind:value={searchDifficulty}
+			class="text-black text-md border-2 border-pink-600 p-2 flex-1"
+		>
+			<option value="*" selected>all difficulties</option>
+			{#each difficulties as difficulty}
+				<option value={difficulty}>{difficulty}</option>
 			{/each}
-		</tbody>
-	</table>
+		</select>
+	</form>
+	<div class="overflow-y-auto overflow-x-hidden col-start-2">
+		<table class="table-fixed w-full">
+			<thead>
+				<tr class="w-full">
+					<th class="px-2 md:px-4 py-2 border-b border-white text-left">Title</th>
+					<th class="px-2 md:px-4 py-2 border-b border-white">Difficulty</th>
+					<th class="px-2 md:px-4 py-2 border-b border-white">Submitted by</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each filteredEntries as [id, title, difficulty, user]}
+					<tr
+						class="w-full border-b border-white transition-colors hover:bg-black hover:bg-opacity-20"
+					>
+						<td>
+							<a href="/reddit/{id}" class="w-full px-2 md:px-4 py-2 flex">{title}</a>
+						</td>
+						<td>
+							<a href="/reddit/{id}" class="w-full px-2 md:px-4 py-2 flex justify-center"
+								>{difficulty}</a
+							>
+						</td>
+						<td>
+							<a href="/reddit/{id}" class="flex justify-center items-center py-1">
+								<a
+									href="https://www.reddit.com/r/hexcellslevels/comments/{id}"
+									class="px-3 py-1 transition-all transform hover:scale-105 hover:bg-yellow-600 hover:bg-opacity-25 rounded-full"
+									target="_blank">{user}</a
+								>
+							</a>
+						</td>
+					</tr>
+				{:else}
+					<tr>
+						<td colspan="3">No puzzles...</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
 </main>
+
+<style lang="postcss">
+	main {
+		@apply grid text-lg h-screen;
+		grid-template-columns: auto 1fr auto;
+		grid-template-rows: auto 1fr;
+	}
+
+	form {
+		@apply flex flex-col items-stretch col-start-2 w-full py-2 gap-2;
+	}
+
+	@screen md {
+		main {
+			@apply text-xl;
+			grid-template-columns: auto minmax(theme("screens.md"), 70%) auto;
+		}
+
+		form {
+			@apply flex-row;
+		}
+	}
+</style>
